03e99d1967a71d8d866906bba5128bbc03b438e0
alecura <alecura@83016304-272c-0410-9067-1d6a512339f2>
2007-04-01 13:59:05 +0000

    Morcion Puzza

--- trunk/README.txt
+++ trunk/README.txt
@@ -2,8 +2,8 @@ Your Game Title
 ===============
 
 Entry in PyWeek #4  <http://www.pyweek.org/4/>
-Team: YOUR TEAM NAME (leave the "Team: bit")
-Members: YOUR TEAM MEMBERS (leave the "Members: bit")
+Team: Pywiii
+Members: dave, hugo, lucio, leito, alecu
 
 
 DEPENDENCIES:
--- trunk/lib/leafs.py
+++ trunk/lib/leafs.py
@@ -28,3 +28,32 @@ class Triangle(qgl.scene.Leaf):
 
     def execute(self):
         glCallList(self.list.id)
+
+class PorcionMuzza(qgl.scene.Leaf):
+    def __init__(self, v):
+        self.vertices = v
+
+    def compile(self):
+        lst = qgl.render.GLDisplayList()
+        glNewList(lst.id, GL_COMPILE)
+
+        v = self.vertices
+        glColorMaterial ( GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE )
+        glEnable ( GL_COLOR_MATERIAL )
+        glBegin(GL_TRIANGLE_STRIP)
+        glColor4f(0.0, 1.0, 1.0, 0.0)
+        glVertex2f(v[0][0],v[0][1])
+        glColor4f(1.0, 1.0, 1.0, 1.0)
+        glVertex2f(v[1][0],v[1][1])
+        glVertex2f(v[2][0],v[2][1])
+        glVertex2f(v[3][0],v[3][1])
+        glVertex2f(v[4][0],v[4][1])
+        glEnd()
+        glColor4f(1.0, 1.0, 1.0, 1.0)
+        glDisable ( GL_COLOR_MATERIAL )
+
+        glEndList()
+        self.list = lst
+
+    def execute(self):
+        glCallList(self.list.id)
