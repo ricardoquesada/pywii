d5c60cb5f20518f48ba35ca8ef3f2fa4efd5051a
lucio.torre <lucio.torre@83016304-272c-0410-9067-1d6a512339f2>
2007-04-05 20:16:22 +0000

    ocho niveles!

--- trunk/lib/simulator.py
+++ trunk/lib/simulator.py
@@ -7,6 +7,8 @@ def getpos(what):
     
 BAR_START = 0
 BAR_END = 1
+MAX_ENERGY = 100
+
 class LevelEnd:
     def __init__(self, win, score):
         self.win = win
@@ -17,6 +19,7 @@ class Simulator(engine.Scene):
     lives = 100
     name = "Level One"
     target = 20
+    energy_step = 2
     
     def init(self, *args, **kwargs):
         self.world = World()
@@ -26,6 +29,7 @@ class Simulator(engine.Scene):
         self.goal = 0
         self.lost = 0
         self.bars = 0
+        self.energy = MAX_ENERGY
         self.stack = []
         
     def get_score(self):
@@ -39,19 +43,25 @@ class Simulator(engine.Scene):
     def event(self, evt):
         if evt.type == KEYDOWN:
             if evt.key == K_ESCAPE:
-                self.end()
+                self.end(LevelEnd(False, 0))
             if evt.key == K_z:
                 self.view.scale(1.0/1.1)
             if evt.key == K_x:
                 self.view.scale(1.1)
         elif evt.type == MOUSEBUTTONDOWN:
-            if evt.button == 1:
-                if self.state == BAR_START:
-                    self.stack.append( evt.pos )
+            print evt.button
+            if evt.button == 3:
+                if self.state == BAR_END:
+                    self.stack.pop()
+                    self.state = BAR_START
+            elif evt.button == 1:
+                if self.state == BAR_START and self.energy>=MAX_ENERGY:
+                    pos = self.view.from_screen(evt.pos)
+                    self.stack.append( pos )
                     self.state = BAR_END
                 elif self.state == BAR_END:
                     start, end = self.new_segment( 
-                        self.view.from_screen(self.stack.pop()),
+                        self.stack.pop(),
                         self.view.from_screen(evt.pos)
                         )
                     if not start==end:
@@ -59,10 +69,13 @@ class Simulator(engine.Scene):
                             Segment( start[0], start[1], end[0], end[1] )
                             )
                         self.bars += 1
+                        self.energy = 0
                     self.state = BAR_START
                 
     def loop(self, dt):
-        self.world.loop(dt/100.0)
+        self.world.loop(0.3)
+        self.energy += self.energy_step
+        
         for evt in self.world.get_events():
             if isinstance(evt, BallAtGoal):
                 self.goal += 1
@@ -75,13 +88,25 @@ class Simulator(engine.Scene):
             
     def update(self):
         self.game.screen.blit( self.background, (0,0) ) 
-        for b in self.world.balls:
-            pygame.draw.circle( 
-                self.game.screen,
-                (255,255,255),
-                self.view.to_screen( getpos(b) ) ,
-                self.view.scale_to_screen( 3 ) , 
-                )
+        
+        for s in self.world.passive:
+            if isinstance(s, Floor):
+                where = self.view.to_screen( (0,s.height) )
+                pygame.draw.rect( 
+                    self.game.screen,
+                    (124,154,255),
+                    (0, where[1], self.game.x_size, self.game.y_size),
+                    0
+                    )
+            elif isinstance(s, Ceiling):
+                where = self.view.to_screen( (0,s.height) )
+                pygame.draw.rect( 
+                    self.game.screen,
+                    (100,100,20),
+                    (0, 0, self.game.x_size, where[1]),
+                    0
+                    )
+                    
         for s in self.world.passive:
             if isinstance(s, LimitedLifeSegment):
                 pygame.draw.line( 
@@ -90,7 +115,7 @@ class Simulator(engine.Scene):
                     self.view.to_screen( s.segment.p1 ) ,
                     self.view.to_screen( s.segment.p2 ) ,
                     )
-            elif isinstance(s, Segment):
+            elif s.__class__ is Segment:
                 pygame.draw.line( 
                     self.game.screen,
                     (255,255,255),
@@ -110,12 +135,18 @@ class Simulator(engine.Scene):
                     self.game.screen,
                     (0,255,255),
                     self.view.to_screen( s.position ) ,
-                    abs(self.view.scale_to_screen( s.force/10 ) ), 
+                    abs(self.view.scale_to_screen( s.force/20 ) ), 
                     )
-                
+        for b in self.world.balls:
+            pygame.draw.circle( 
+                self.game.screen,
+                (255,255,255),
+                self.view.to_screen( getpos(b) ) ,
+                self.view.scale_to_screen( 3 ) , 
+                )        
         if self.state == BAR_END:
             start, end = self.new_segment( 
-                    self.view.from_screen( self.stack[-1] ),
+                    self.stack[-1],
                     self.view.from_screen( pygame.mouse.get_pos() ),
                    )
             start = self.view.to_screen( start )
@@ -130,18 +161,33 @@ class Simulator(engine.Scene):
         self.log("lost:   "+str(self.lost))
         self.log("alive:  "+str(len(self.world.balls)))
         self.log("fps:    "+str( self.game.clock.get_fps() ))
+        if self.energy >= MAX_ENERGY:
+            self.log("energy: READY!")
+        else:
+            self.log("energy: "+str( self.energy ))
     
     
+    def can_place(self, start, end):
+        for s in self.world.passive:
+            if isinstance(s, Ceiling):
+                print s.height, start[1], end[1], s.height <= max(start[1], end[1])
+                if s.height <= max(start[1], end[1]):
+                    return False
+        return True
+            
     def new_segment(self, start, end):
         if start == end: return start, start+Vector2(100,0)
         s = LineSegment2( Point2(*start), Point2(*end) )
         v = s.v.normalized()
         v *= 100
-        return s.p1, s.p1+v
+        start, end = s.p1, s.p1+v
+        if not self.can_place(start, end):
+            return (0,0),(0,0)
+        return start, end
     
     
 class LevelOne(Simulator):
-    lives = 100
+    lives = 50
     name = "Level One"
     target = 20
     
@@ -152,7 +198,7 @@ class LevelOne(Simulator):
         self.world.add_passive( Floor(-200) )
 
 class LevelTwo(Simulator):
-    lives = 100
+    lives = 25
     name = "Level Two"
     target = 20
     
@@ -163,7 +209,7 @@ class LevelTwo(Simulator):
         self.world.add_passive( Floor(-200) )
 
 class LevelThree(Simulator):
-    lives = 100
+    lives = 20
     name = "Level Three"
     target = 20
     
@@ -176,22 +222,92 @@ class LevelThree(Simulator):
         self.world.add_passive( Floor(-200) )
 
 class LevelFour(Simulator):
-    lives = 100
+    lives = 20
     name = "Level Four"
     target = 20
     
     def setup_level(self):
         self.world.add_active( Generator((0,10)) )
         self.world.add_passive( Segment(-100,20,100,20) )
-        self.world.add_passive( LimitedLifeSegment(-100,20,0,100, life=5) )
-        self.world.add_passive( LimitedLifeSegment(0,100,100,20, life=5) )
-        self.world.add_attractor( Attractor(-100,20, force=-50) )
-        self.world.add_attractor( Attractor(5,-50, force=50) )
-        self.world.add_attractor( Attractor(100,20, force=-50) )
         self.world.add_passive( Goal(0,60,15.) )
         self.world.add_passive( Floor(-200) )
+        self.world.add_passive( Ceiling(00) )
         
+class LevelFive(Simulator):
+    lives = 30
+    name = "Level Five"
+    target = 20
+    
+    def setup_level(self):
+        self.world.add_active( Generator((0,10)) )
+        self.world.add_passive( Segment(-100,20,100,20) )
+        self.world.add_attractor( Attractor(-100,20, force=-70) )
+        self.world.add_attractor( Attractor(5,-50, force=50) )
+        self.world.add_attractor( Attractor(100,20, force=-70) )
+        self.world.add_passive( Goal(0,60,15.) )
+        self.world.add_passive( Floor(-200) )
+
+class LevelSix(Simulator):
+    lives = 50
+    name = "Level Six"
+    target = 20
+    
+    def setup_level(self):
+        self.world.add_active( Generator((-125,10)) )
+        self.world.add_active( Generator((125,10)) )
         
+        self.world.add_passive( Segment(-150,100,150,100) )
+        self.world.add_passive( Goal(0,150,15.) )
+        self.world.add_passive( Floor(-200) )
+        
+class LevelSeven(Simulator):
+    lives = 100
+    name = "Level Seven"
+    target = 20
+    
+    def setup_level(self):
+        self.world.add_active( Generator((-125,10)) )
+        self.world.add_active( Generator((125,10)) )
+        
+        self.world.add_passive( Segment(-150,100,150,100) )
+        self.world.add_attractor( Attractor(-125,-50, force=-50) )
+        self.world.add_attractor( Attractor(-75,-50, force=50) )
+        self.world.add_attractor( Attractor(-25,-50, force=50) )
+        self.world.add_attractor( Attractor(0,-150, force=-50) )        
+        self.world.add_attractor( Attractor(25,-50, force=50) )
+        self.world.add_attractor( Attractor(75,-50, force=50) )
+        self.world.add_attractor( Attractor(125,-50, force=-50) )
+        self.world.add_passive( Goal(0,150,15.) )
+        self.world.add_passive( Floor(-200) )
+
+class LevelEight(Simulator):
+    lives = 100
+    name = "Level Eight"
+    target = 20
+    
+    def setup_level(self):
+        self.world.add_active( Generator((-75,10)) )
+        self.world.add_active( Generator((75,10)) )
+        
+        self.world.add_passive( Segment(-150,100,150,100) )
+        self.world.add_passive( Goal(0,150,15.) )
+        self.world.add_passive( Floor(-200) )
+        self.world.add_passive( Ceiling(150) )
+        w = 50
+        xbase = -150
+        xstep = 150
+        ybase = -200
+        ystep = 30
+        for x in range(3):
+            for y in range(10):
+                self.world.add_passive( LimitedLifeSegment(
+                    xbase+xstep*x-w,
+                    ybase+ystep*y,
+                    xbase+xstep*x+w,
+                    ybase+ystep*y,
+                    life=2) )
+        
+
 class Runner(engine.Scene):
     log_font_size = 40
     PRE_LEVEL = 0
@@ -204,6 +320,10 @@ class Runner(engine.Scene):
             LevelTwo,
             LevelThree,
             LevelFour,
+            LevelFive,
+            LevelSix,
+            LevelSeven,
+            LevelEight,
         ]
     def init(self):
         self.state = self.PRE_LEVEL
