dda8560c53c14e2e37da685931892b1d70fd7897
alecura <alecura@83016304-272c-0410-9067-1d6a512339f2>
2007-04-06 17:26:47 +0000

    levantar el menu al correr el juego

---
+++ run_game.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python
+
+import sys
+import os
+try:
+    libdir = os.path.abspath(os.path.join(os.path.dirname(__file__), 'lib'))
+    sys.path.insert(0, libdir)
+except:
+    # probably running inside py2exe which doesn't set __file__
+    pass
+
+
+from game import Game
+from levels import *
+
+if __name__ == '__main__':
+    g = Game()
+    g.change_scene(view.MainMenu(g))
+    g.main_loop()
